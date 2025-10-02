3ddc12cbde131f5f637519bf5dce8aeb3bad0e98
wentao-tao <wentao.emiya@gmail.com>
2025-10-02 10:38:26 +0800

    Initial Commit

---
+++ unmute/unmuteApp.swift
@@ -0,0 +1,17 @@
+//
+//  unmuteApp.swift
+//  unmute
+//
+//  Created by Wentao Guo on 02/10/25.
+//
+
+import SwiftUI
+
+@main
+struct unmuteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
